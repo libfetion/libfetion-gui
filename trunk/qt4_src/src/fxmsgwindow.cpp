d35e6b2a19088ee148e092438e881d57f2f3410c
libfetion <libfetion@3dff0f8e-6350-0410-ad39-8374e07577ec>
2009-05-12 11:53:31 +0000

    included  <math.h>  fxmsgwindow.cpp

--- trunk/qt4_src/src/fxmsgwindow.cpp
+++ trunk/qt4_src/src/fxmsgwindow.cpp
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <math.h>
 #include "fxmainwindow.h"
 #include "fxmsgwindow.h"
 #include "fxutil.h"
