98d556e5fc9ecc8a6292e6a3af6a33a76e667094
libfetion <libfetion@3dff0f8e-6350-0410-ad39-8374e07577ec>
2009-11-16 14:07:47 +0000

    gui: fix the resource placed to resource catalog.

--- qt4_src/install.sh
+++ qt4_src/install.sh
@@ -11,8 +11,7 @@ sudo cp ./misc/fetion.png /usr/share/app-install/icons/
 sudo chmod 644 /usr/share/app-install/icons/fetion.png
 
 sudo mkdir -p /usr/share/libfetion/
-sudo cp -rf README COPYING ChangeLog CREDITS.txt skins/ faces_image/ /usr/share/libfetion/
-sudo cp -rf fetion_utf8_CN.qm qt_zh_CN.qm sound/ data/ /usr/share/libfetion/
+sudo cp -rf README COPYING ChangeLog CREDITS.txt resource /usr/share/libfetion/
 sudo chmod -R 755 /usr/share/libfetion/
 
 sudo mkdir -p /usr/share/applications/ 
