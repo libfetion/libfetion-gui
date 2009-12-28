20cd0bed0682ba0dcaa3d9fd4209ea2dd2e58ef4
libfetion@gmail.com <libfetion@gmail.com@3dff0f8e-6350-0410-ad39-8374e07577ec>
2009-12-28 14:18:49 +0000

    pro: update app version for 1.3

--- trunk/qt4_src/src/appconfig.h
+++ trunk/qt4_src/src/appconfig.h
@@ -20,9 +20,9 @@
 #ifndef FXAPPCONFIG_H 
 #define FXAPPCONFIG_H 
 
-#define BUILD_DATE 20091125
-#define CURRENT_VERSION 102
-#define VERSION_NO "v1.2"
+#define BUILD_DATE 20091228
+#define CURRENT_VERSION 130
+#define VERSION_NO "v1.3"
 
 #define DefaultSkinCatalog "Fx_Kieteay"
 #define DefaultSkinName "Fx_Kieteay"
